e2db69524ef149a0e99bd502530b6cc12aa771a5
Justin Choi <JChoi@stu.kerikerihigh.ac.nz>
2021-08-09 13:06:38 +1200

    good

---
+++ inlist.py
@@ -0,0 +1,2 @@
+correct_answer = 'True', 'False'
+print(correct_answer.capitalize())
--- quiz.py
+++ quiz.py
@@ -1,28 +1,36 @@
 import requests
 from html import unescape
 
-response = requests.get("https://opentdb.com/api.php?amount=15&category=23&difficulty=easy&type=boolean")
+response = requests.get("https://opentdb.com/api.php?amount=5&category=23&difficulty=easy&type=boolean")
 
 questions = response.json()["results"]
-
-print('\033[95m'"""------------welcome to the wild no heros 's villains------------
+# this is a intro/rules for the quiz
+print('\033[95m'"""------------welcome to the history quiz------------
     hi, my name is Justin, i made this quiz by using my python skills, i hope you enjoy! 
     Now, this is how to play my quiz you read the question and then answer either True or False. 
     (make sure your spelling is correct and use capital letter for the first letter of the answer) enjoy ^;^""")
+# this is to print the questions on the screen
 score = 0
 for i, question in enumerate(questions):
     print(f"--------------question {i + 1}--------------")
     ans = input(unescape(question["question"]))
 
-    if ans == question["correct_answer"]:
+    ans_capital = ans.capitalize()
+# this tells the users whether they got the question right or not
+    if ans_capital in question["correct_answer"]:
         print("correct!!!")
         score = score + 1
     else:
         print(f"incorrect, the correct answer was ",question["correct_answer"])
         
 
-
+# shows the total scores
 score = int(score)
+print(f"your score is {score} out of 5")
+# this tells the scores for the users and saying goodbye
+print("Thanks for playing!")             
 
-print(f"your score is {score} out of 15")
-print("Thanks for playing!")
+#TODO 
+# Let user input True true T t yes Yes y Y False false F f No no n N .lower()
+# Don't let the use put in any other input
+# add a way to get different questions
